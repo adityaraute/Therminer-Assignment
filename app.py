0d4d2aae745774aceeab9b789446f4e10e2b5346
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2024-11-08 12:23:17 +0000

    Random data vis generated

--- app.py
+++ app.py
@@ -3,36 +3,13 @@ import pandas as pd
 import plotly.express as px
 from plotly import utils
 import json
+import numpy as np
 
+#App config
 app = Flask(__name__)
 app.debug = True
 
-def generate_figure(request):
-    selected_option = request.form.get('SelectVis')
-
-    if selected_option == 'linear':
-        data = {
-            'Name': ['A', 'B', 'C', 'D'],
-            'Value': [10, 15, 7, 12]
-    }
-    elif selected_option == 'random':
-        data = {
-            'Name': ['E', 'F', 'G', 'H'],
-            'Value': [100, 15, 7, 12]
-    }
-    else:
-        return "Error occured in form submission"
-
-    df = pd.DataFrame(data)
-
-    # Create a Plotly figure
-    fig = px.bar(df, x='Name', y='Value', title=df.iloc[0]['Name'])
-
-    # Convert the figure to JSON format
-    graphJSON = json.dumps(fig, cls=utils.PlotlyJSONEncoder)
-
-    return graphJSON
-
+#Routing
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     parameter = None
@@ -41,3 +18,46 @@ def index():
         parameter = generate_figure(request)
 
     return render_template('index.html', params = parameter)
+
+
+#Ancillary functions
+def generate_figure(request):
+    selected_option = request.form.get('SelectVis')
+
+    #Pass Linear form parameters to a function
+    if selected_option == 'linear':
+        data = generate_linear_data(request.form.get('slope'), request.form.get('intercept'))
+
+    elif selected_option == 'random':
+        data = generate_random_data(request.form.get('min'), request.form.get('max'))
+
+    else:
+        return "Error occured in form submission"
+
+    df = data
+
+    # Create a Plotly figure
+    fig = px.bar(df, x='Date', y='Value', title="Graph")
+
+    # Convert the figure to JSON format
+    graphJSON = json.dumps(fig, cls=utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+def generate_linear_data():
+    #Generate y = mt + c data
+    pass
+
+def generate_random_data(minval, maxval):
+    #Generate random data between min-max boundaries
+
+    date_range = pd.date_range(start='2024-11-07', end='2024-11-07 23:59', freq='10min')
+
+    random_values = np.random.randint(minval, maxval, size=len(date_range))
+    print(date_range, random_values)
+    time_series_data = pd.DataFrame({'Date': date_range, 'Value': random_values})
+
+    print(time_series_data.head())
+    
+    return time_series_data
+
