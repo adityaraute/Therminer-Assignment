2f970a57561713b708ebbe76d6dbc2b763ff6274
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2024-11-08 11:33:51 +0000

    Sample Plotly div added

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import plotly.express as px
+from plotly import utils
+import json
 
 app = Flask(__name__)
 app.debug = True
 
 def generate_figure(request):
     selected_option = request.form.get('SelectVis')
+
     if selected_option == 'linear':
-        pass
+        data = {
+            'Name': ['A', 'B', 'C', 'D'],
+            'Value': [10, 15, 7, 12]
+    }
     elif selected_option == 'random':
-        pass
+        data = {
+            'Name': ['E', 'F', 'G', 'H'],
+            'Value': [100, 15, 7, 12]
+    }
     else:
         return "Error occured in form submission"
 
-    return selected_option + request.form.get('min')
+    df = pd.DataFrame(data)
+
+    # Create a Plotly figure
+    fig = px.bar(df, x='Name', y='Value', title=df.iloc[0]['Name'])
+
+    # Convert the figure to JSON format
+    graphJSON = json.dumps(fig, cls=utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 @app.route("/", methods = ['GET', 'POST'])
-
-def hello():
-    parameter = ""
+def index():
+    parameter = None
 
     if request.method == 'POST':
         parameter = generate_figure(request)
 
     return render_template('index.html', params = parameter)
-
-
-'''
-data = generate_report()
-return render_template("report.html", chart_data=data)
-'''
