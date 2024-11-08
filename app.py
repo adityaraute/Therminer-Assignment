e478696dd34427511ee7d374b951f32cb8c1129c
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2024-11-08 09:26:19 +0000

    Added plotly CDN

--- app.py
+++ app.py
@@ -3,17 +3,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.debug = True
 
+def generate_figure(request):
+    selected_option = request.form.get('SelectVis')
+    if selected_option == 'linear':
+        pass
+    elif selected_option == 'random':
+        pass
+    else:
+        return "Error occured in form submission"
+
+    return selected_option + request.form.get('min')
+
 @app.route("/", methods = ['GET', 'POST'])
+
 def hello():
     parameter = ""
+
     if request.method == 'POST':
-        parameter = request.form.get('SelectVis')
-        print(request.form.get('SelectVis'))
+        parameter = generate_figure(request)
+
     return render_template('index.html', params = parameter)
 
 
 '''
 data = generate_report()
 return render_template("report.html", chart_data=data)
-
 '''
