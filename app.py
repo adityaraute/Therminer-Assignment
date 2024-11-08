bb64887a3bcb36fc75e6037f50e5cb70bf14d0af
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2024-11-08 05:03:01 +0000

    Add debug instruction

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('figure.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
